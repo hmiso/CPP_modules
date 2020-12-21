c645296481eae99fc069adb79aea8c9705fa3c01
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-12-21 23:48:34 +0300

    find and replace

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -3,31 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmiso <hmiso@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hmiso <hmiso@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 19:50:14 by hmiso             #+#    #+#             */
-/*   Updated: 2020/12/21 20:17:15 by hmiso            ###   ########.fr       */
+/*   Updated: 2020/12/21 23:46:37 by hmiso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include <fstream>
 
 int main(int argc, char **argv)
 {
 	std::ifstream in;
 	std::ofstream out;
+	std::string str = argv[1] + ".REPLASE";
+	std::string str1 = "Hello";
+	std::string str2 = "world";
 	if (argc != 4)
 	{
 		std::cout << "Wrong argument!" << std::endl;
 		exit(0);
 	}
-	out.open("test.replace");
+	out.open(str);
 	in.open(argv[1]);
 	if (!in.is_open() || !out.is_open())
 	{
 		std::cout << "Bad file!" << std::endl;
 		exit(0);
 	}
-	out << "Good" << std::endl;
+
+	str1 = argv[2];
+	str2 = argv[3];
+	while(str.find(str1) != -1)
+		str.replace(str.find(str1), str2.size(), str2);
+	std::cout << str << std::endl;
 }
